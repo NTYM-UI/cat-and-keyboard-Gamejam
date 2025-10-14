0ec7fc3b5b8473abddcb484c6688362c6aad326e
ほしの　しずく <1694015875@qq.com>
2025-10-14 19:25:58 +0800

    Add files via upload

---
+++ Assets/_Project/01_Scripts/Gameplay/Other/SaveData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData 
+{
+    public float playerPosX; //玩家X坐标
+    public float playerPosY; //玩家Y坐标
+    public int currentLevel; //当前关卡
+
+}
